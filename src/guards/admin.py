72f8105f4ad1ab3f2e76c2c849333f710d356ab7
Andrii Antonenko <andriyantonenko3.16@gmail.com>
2025-11-20 00:52:15 +0200

    feat: admin guard decorator

---
+++ src/guards/admin.py
@@ -0,0 +1,19 @@
+from functools import wraps
+from ..services.users import UsersService
+from ..loader import bot
+from telebot.types import Message, CallbackQuery
+
+def admin_guard(func):
+  users_service = UsersService()
+
+  @wraps(func)
+  async def wrapper(call_or_message, *args, **kwargs):
+    tg_user_id = call_or_message.from_user.id
+    chat_id = call_or_message.message.chat.id if isinstance(call_or_message, CallbackQuery) else call_or_message.chat.id
+    user = await users_service.get_user_if_exists(tg_user_id)
+    if user is None or not user.is_admin:
+      await bot.send_message(chat_id, "🚫 You do not have permission to access admin commands.")
+      return
+    return await func(call_or_message, *args, **kwargs)
+  
+  return wrapper
--- src/handlers/admin.py
+++ src/handlers/admin.py
@@ -5,34 +5,19 @@ from ..services.users import UsersService, User
 from ..keyboards.inline import admin_keyboard, pending_orders_keyboard, admin_order_commands_keyboard
 from ..services.orders import OrdersService, OrderStatus
 from ..constants import VIEW_PENDING_ORDERS_CQ, VIEW_ORDER_DETAILS_CQ_PREFIX, ADMIN_CMD, APPROVE_ORDER_CQ_PREFIX, CANCEL_ORDER_CQ_PREFIX
-
-async def check_admin(user_id: int) -> User | None:
-  users_service = UsersService()
-  user = await users_service.get_user_if_exists(user_id)
-  return user is not None and user
-
-async def send_admin_guard_message(chat_id):
-  await bot.send_message(chat_id, "🚫 You do not have permission to access admin commands.")
+from ..guards.admin import admin_guard
 
 @bot.message_handler(commands=[ADMIN_CMD])
+@admin_guard
 async def cmd_admin(message: Message):
-  admin = await check_admin(message.from_user.id);
-  if not admin:
-    await send_admin_guard_message(message)
-    return
-  
   await bot.send_message(message.chat.id, '''Hello @{username}!
 Welcome to the admin panel. Use the buttons below to manage pending orders.'''.format(username=message.from_user.username),
                    reply_markup=admin_keyboard())
 
 
 @bot.callback_query_handler(func=lambda call: call.data == VIEW_PENDING_ORDERS_CQ)
+@admin_guard
 async def handle_view_pending_orders(call):
-  admin = await check_admin(call.from_user.id);
-  if not admin:
-    await send_admin_guard_message(call.message.chat.id)
-    return
-  
   orders_service = OrdersService()
   pending_orders_count = await orders_service.get_pending_orders_count()
   if pending_orders_count == 0:
@@ -44,12 +29,8 @@ async def handle_view_pending_orders(call):
   
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith(VIEW_ORDER_DETAILS_CQ_PREFIX))
+@admin_guard
 async def handle_order_details(call):
-  admin = await check_admin(call.from_user.id);
-  if not admin:
-    await send_admin_guard_message(call.message.chat.id)
-    return
-  
   order_id_str = call.data.removeprefix(VIEW_ORDER_DETAILS_CQ_PREFIX)
   order_id = int(order_id_str) if order_id_str.isdigit() else 0
   if order_id == 0:
@@ -73,12 +54,8 @@ Created At: {order.created_at}'''
   await bot.send_message(call.message.chat.id, msg, reply_markup=admin_order_commands_keyboard(order.id))
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith(APPROVE_ORDER_CQ_PREFIX))
+@admin_guard
 async def handle_approve_order(call):
-  admin = await check_admin(call.from_user.id);
-  if not admin:
-    await send_admin_guard_message(call.message.chat.id)
-    return
-  
   order_id_str = call.data.removeprefix(APPROVE_ORDER_CQ_PREFIX)
   order_id = int(order_id_str) if order_id_str.isdigit() else 0
   if order_id == 0:
@@ -95,12 +72,8 @@ async def handle_approve_order(call):
   await bot.send_message(call.message.chat.id, f"Order #{order_id} has been marked as completed.", reply_markup=admin_keyboard())  
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith(CANCEL_ORDER_CQ_PREFIX))
+@admin_guard
 async def handle_cancel_order(call):
-  admin = await check_admin(call.from_user.id);
-  if not admin:
-    await send_admin_guard_message(call.message.chat.id)
-    return
-  
   order_id_str = call.data.removeprefix(CANCEL_ORDER_CQ_PREFIX)
   order_id = int(order_id_str) if order_id_str.isdigit() else 0
   if order_id == 0:
